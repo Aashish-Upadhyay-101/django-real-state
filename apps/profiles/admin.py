c3cc204bfdb55ec996a648a64655656fe8231473
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2022-09-30 20:13:31 +0545

    feat: add profile model and signals

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from .models import Profile
 
-# Register your models here.
+
+class ProfileAdmin(ModelAdmin):
+    list_display = ["id", "pkid", "gender", "user", "phone_number", "country", "city"]
+    list_filter = ["gender", "country", "city"]
+    list_display_links = ["id", "pkid", "user"]
+
+admin.site.register(Profile, ProfileAdmin)
+
+

3f129c1f9b7597fe8604f59fba788b7b758b4cbe
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2022-10-13 01:40:11 +0545

    feat: user and property slice

---
+++ client/src/features/auth/authSlice.js
@@ -0,0 +1,115 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { errorMessage } from "../../helpers/errorMessage";
+import authService from "./authService";
+
+const user = JSON.parse(localStorage.getItem("user"));
+
+const initialState = {
+  user: user ? user : null,
+  isLoading: false,
+  isError: false,
+  isSuccess: false,
+  message: "",
+};
+
+const register = createAsyncThunk("auth/register", async (user, thunkAPI) => {
+  try {
+    return await authService.register(user);
+  } catch (error) {
+    const message = errorMessage(error);
+    return thunkAPI.rejectWithValue(message);
+  }
+});
+
+const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+  try {
+    return await authService.login(user);
+  } catch (error) {
+    const message = errorMessage(error);
+    return thunkAPI.rejectWithValue(message);
+  }
+});
+
+const logout = createAsyncThunk("auth/logout", async () => {
+  authService.logout();
+});
+
+const activate = createAsyncThunk("auth/activate", async (user, thunkAPI) => {
+  try {
+    return await authService.activate(user);
+  } catch (error) {
+    const message = errorMessage(error);
+    return thunkAPI.rejectWithValue(message);
+  }
+});
+
+// auth slice
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isError = false;
+      state.isLoading = false;
+      state.isSuccess = false;
+      state.message = "";
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      // register
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isSuccess = true;
+        state.isLoading = false;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isError = true;
+        state.isLoading = false;
+        state.user = null;
+        state.message = action.payload;
+      })
+
+      // login
+      .addCase(login.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.isError = true;
+        state.isLoading = false;
+        state.user = null;
+        state.message = action.payload;
+      })
+
+      // logout
+      .addCase(logout.fulfilled, (state) => {
+        state.user = null;
+      })
+
+      // activate
+      .addCase(activate.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(activate.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+      })
+      .addCase(activate.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        state.user = null;
+      });
+  },
+});
+
+export const { reset } = authSlice.actions;
+export default authSlice.reducer;
---
+++ client/src/helpers/errorMessage.js
@@ -0,0 +1,10 @@
+const errorMessage = (error) => {
+  const message =
+    (error.response && error.response.data && error.response.data.message) ||
+    error.message ||
+    error.toString();
+
+  return message;
+};
+
+export { errorMessage };

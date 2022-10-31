b34a5a3d4e67ae632be019893512202eb0c1e5ba
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2022-11-01 00:30:59 +0545

    feat: setup docker

--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -160,7 +160,7 @@ AUTH_USER_MODEL = "users.User"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
 }
 
 from datetime import timedelta
